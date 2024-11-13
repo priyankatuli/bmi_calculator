9c1a885d0432779e2e1eb57a6548c05e26a9818e
priyankatuli <priyankaaatuli123@gmail.com>
2024-11-13 14:31:00 +0600

    some code changed

--- lib/ui/screens/bmi_findout_screen.dart
+++ lib/ui/screens/bmi_findout_screen.dart
@@ -1,4 +1,5 @@
 import 'package:bmi_calculator/controllers/theme_controller.dart';
+import 'package:bmi_calculator/ui/utils/text_strings.dart';
 import 'package:bmi_calculator/ui/widgets/bmi_category.dart';
 import 'package:bmi_calculator/ui/widgets/section_header.dart';
 import 'package:flutter/material.dart';
@@ -19,28 +20,28 @@ class BmiFindOutScreen extends StatelessWidget{
                  mainAxisAlignment: MainAxisAlignment.start,
                  children: [
                    const SizedBox(height: 30,),
-                   const Text('Body Mass Index',style: TextStyle(
+                   const Text(TextStrings.bmiTitle,style: TextStyle(
                      fontSize: 24,
                      fontWeight: FontWeight.bold
                    ),),
                   const SizedBox(height: 10,),
-                   Text('Body Mass Index, or BMI, is a quick way to check if a person’s weight is healthy for their height.'
-                       'Its calculated by dividing a persons weight by the square of their height. The result gives a number that falls into different categories: underweight, normal weight, overweight, or obese.',
+                   Text(TextStrings.description,
                          style: TextStyle(
                            color: themeController.isDarkMode ? Colors.white : Colors.black,
+
                          ),
                        ),
                    const SizedBox(height: 10,),
                   const  Row(
                     mainAxisAlignment: MainAxisAlignment.start,
                        children: [
-                         SectionHeader(text: "BMI Formula: ") ]
+                         SectionHeader(text: TextStrings.bmiText1) ]
            ),
                    const SizedBox(height: 15,),
                    const Row(
                      mainAxisAlignment: MainAxisAlignment.center,
                        children : [
-                         Text('BMI = (Weight in Kg) / (Height in meters ( m^2 ) )',
+                         Text(TextStrings.bmiFormula,
                            style: TextStyle(
                                fontWeight: FontWeight.bold,
                                letterSpacing: 0.5
@@ -51,7 +52,7 @@ class BmiFindOutScreen extends StatelessWidget{
                    const Row(
                      mainAxisAlignment: MainAxisAlignment.start,
                        children : [
-                        SectionHeader(text: "BMI Chart is: ")
+                        SectionHeader(text: TextStrings.bmiText2)
                        ]
                    ),
                            const Column(
@@ -59,12 +60,12 @@ class BmiFindOutScreen extends StatelessWidget{
                              children: [
                                SizedBox(height: 10,),
                                Wrap(children: [
-               BmiCategory(text: "If BMI range is below from 18.5 then its Underweight."),
-               BmiCategory(text: "If BMI range is below from 24.9 then its Normal Weight."),
-               BmiCategory(text: "If BMI range is below from 29.9 then its Overweight."),
-               BmiCategory(text: "If BMI range is below from 34.9 then its 1st Obesity."),
-               BmiCategory(text: "If BMI range is below from 39.9 then its 2nd Obesity."),
-               BmiCategory(text: "If BMI range is greater than 40 or above  then its Extreme Obesity."),
+               BmiCategory(text: TextStrings.bmiRange1),
+               BmiCategory(text: TextStrings.bmiRange2),
+               BmiCategory(text: TextStrings.bmiRange3),
+               BmiCategory(text: TextStrings.bmiRange4),
+               BmiCategory(text: TextStrings.bmiRange5),
+               BmiCategory(text: TextStrings.bmiRange6),
                                ],)
                              ]
                            )
--- lib/ui/utils/text_strings.dart
+++ lib/ui/utils/text_strings.dart
@@ -9,5 +9,16 @@ class TextStrings {
   static const String age = "Age";
   static const String defaultWeight = "45";
   static const String defaultAge = "25";
+  static const String description = "Body Mass Index, or BMI, is a quick way to check if a person’s weight is healthy for their height.Its calculated by dividing a persons weight by the square of their height. The result gives a number that falls into different categories: underweight, normal weight, overweight, or obese.";
+  static const String bmiTitle = "Body Mass Index";
+  static const String bmiText1 = "BMI Formula";
+  static const String bmiText2 = "BMI Chart is";
+  static const String bmiFormula = "BMI = (Weight in Kg) / (Height in meters ( m^2 ) )";
+  static const String bmiRange1 = "If BMI range is below from 18.5 then its Underweight.";
+  static const String bmiRange2 = "If BMI range is below from 24.9 then its Normal Weight";
+  static const String bmiRange3 = "If BMI range is below from 29.9 then its Overweight.";
+  static const String bmiRange4 = "If BMI range is below from 34.9 then its 1st Obesity.";
+  static const String bmiRange5= "If BMI range is below from 39.9 then its 2nd Obesity.";
+  static const String bmiRange6= "If BMI range is greater than 40 or above  then its Extreme Obesity.";
 
 }
